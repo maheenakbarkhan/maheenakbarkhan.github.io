2e782001cdd923d17056f9ecfbfcdd163ef4fad2
Maheen Khan <maheenkhan@Maheens-MacBook-Pro.local>
2022-02-24 13:12:34 -0600

    adding some html tags to break up the descriptions into paragraphs

--- config.js
+++ config.js
@@ -16,13 +16,13 @@ var config = {
             hidden: false,
             title: 'Vietnam (1955 - 1975)',
             image: '',
-            description: 'Jon Burge and a generation of police officers enlist in the US Military for the Vietnam War. ' + 
+            description: 'Jon Burge and a generation of police officers enlist in the US Military for the Vietnam War.<br/><br/> ' + 
             'In 1968, Jon Burge volunteered for duty in the Vietnam War, where he provided security for convoys and basecamps ' + 
             'as a military police officer, and served a tour as a provost marshal investigator. Vietnam was his second service ' + 
             '(he had previously worked as military police in South Korea) and he was assigned to the Ninth Military ' +
             'Police Company of the Ninth Infantry division.', 
             location: {
-                center: [108.33127, 14.28268],
+                center: [108.33036, 14.37008],
                 zoom: 6.17,
                 pitch: 0,
                 bearing: 0
@@ -77,7 +77,7 @@ var config = {
             'Officers Fahey and O’Brien. They were tortured into confession; both brothers were connected to a ' +
             'hand-cranked generator and given electric shocks. Their legal fights lasted decades: Andrew Wilson ' +
             'ultimately died in prison in 2007. Jackie Wilson was released in 2018 and granted a certificate of ' +
-            'innocence in December 2020',
+            'innocence in December 2020.',
             location: {
                 center: [-87.60398, 41.72948],
                 zoom: 17.4,
@@ -101,7 +101,7 @@ var config = {
             'load a shotgun and stick it in his mouth, forcing him to pull the trigger. The officers then pulled ' +
             'down his pants and repeatedly shocked him in the genitals with a cattle prod. “For me this was ' +
             'the most sadistic act ever performed,” he told journalist Amanda Rivkin in 2016. “The feeling of ' +
-            'it is something that is indescribable. I still live with it today.” Sgt. John Byrne and Detectives ' +
+            'it is something that is indescribable. I still live with it today.”<br/><br/> Sgt. John Byrne and Detectives ' +
             'Peter Dignan, who worked closely with Burge and were both Marine Corps veterans, participated in ' +
             'torturing and coercing his murder confession. Cannon was exonerated in 2004 and freed in 2007. His case ' +
             'became a touchpoint for other Burge-related cases. Today, he organizes fellow survivors in the ' +
@@ -146,7 +146,7 @@ var config = {
             hidden: false,
             title: 'Area 3 Police Station (1988 - 1991)',
             image: '',
-            description: 'Burge moved to Area 3 in 1988. One of Burge’s contemporaries, Richard Zuley, ' +
+            description: 'Burge moved to Area 3 in 1988.<br/><br/>One of Burge’s contemporaries, <b>Richard Zuley</b>, ' +
             'also joined the Chicago Police in 1970. Zuley,  who is most infamous for his direct link ' +
             'to torture at Guantanamo, took a leave of absence from CPD from 1982-1987 to work in counterterrorism ' +
             'for the U.S. Navy. It’s unclear what his counterterrorism work included, where he was ' +
@@ -172,12 +172,12 @@ var config = {
             description: 'When he returned to Chicago, both Burge and Zuley briefly overlapped on the ' +
             'North Side: Burge in Unit 630, Area 3 (within what is now called Detective Area North), and ' +
             'Zuley in Unit 606, Central Investigations.  Burge was fired in 1993, after the Chicago ' +
-            'Police Board found that he had abused Andrew Wilson. Although it is not in official records that ' +
+            'Police Board found that he had abused Andrew Wilson.<br/><br/>Although it is not in official records that ' +
             'they ever worked together, Zuley and Burge shared mutual colleagues. Zuley was seen as an ' +
             'expert in his 25-year career as a detective, investigating high profile crimes like the murder ' +
-            'of 7-year old Dantrell Davis. Over that same career, he tortured at least five people in ' +
-            'Chicago. At Area 3, he tortured Lee Harris in 1989, who was once Zuley’s close informant, and Benita ' +
-            'Johnson in 1995, whom he handcuffed to a wall for over 24 hours and threatened her family.',
+            'of 7-year old Dantrell Davis.<br/><br/>Over that same career, he tortured at least five people in ' +
+            'Chicago. <b>At Area 3, he tortured Lee Harris in 1989, who was once Zuley’s close informant, and Benita ' +
+            'Johnson in 1995, whom he handcuffed to a wall for over 24 hours and threatened her family.</b>',
             location: {
                 center: [-87.74632, 41.92242],
                 zoom: 18,
@@ -201,7 +201,7 @@ var config = {
             'cases—training and consulting in private military-esque firms in their retirement. These officers ' +
             'have not admitted culpability, decades later. Boudreau, for example, is named in at least 46 ' +
             'torture claims, thirteen of them alongside now-deceased Michael Kill (between the two of them, ' +
-            '72 people have accused them of using torture; 20 of whom have been exonerated). Boudreau took a ' +
+            '72 people have accused them of using torture; 20 of whom have been exonerated).<br/><br/>Boudreau took a ' +
             'leave from the CPD, to serve in the Gulf War from September 1990-July 1991. He worked as a Protective ' +
             'Service Officer and Aide to Camp for the Commanding General of Engineer Forces.',
             location: {
