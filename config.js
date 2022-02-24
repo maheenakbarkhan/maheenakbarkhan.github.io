dc716f17e45d89f3607b55b644681131cee92868
Maheen Khan <maheenkhan@Maheens-MacBook-Pro.local>
2022-02-24 11:49:35 -0600

    adding chapters

--- config.js
+++ config.js
@@ -5,23 +5,27 @@ var config = {
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
-    title: 'Example Story',
-    subtitle: 'I am making an example of a story to test this',
-    byline: 'By Maheen Khan',
+    title: 'Chicago to Guantánamo, Connections in an Ecosystem of Violence',
+    subtitle: '',
+    byline: 'StoryMap by Maira Khwaja, Marie Mendoza, and Maheen Khan',
     footer: 'Source: source citations, etc.',
     chapters: [
         {
-            id: 'invisible-institute-id',
+            id: 'vietnam',
             alignment: 'left',
             hidden: false,
-            title: 'Invisible Institute',
+            title: 'Vietnam (1955-1975)',
             image: '',
-            description: 'The Invisible Institute Office',
+            description: 'Jon Burge and a generation of police officers enlist in the US Military for the Vietnam War. ' + 
+            'In 1968, Jon Burge volunteered for duty in the Vietnam War, where he provided security for convoys and basecamps ' + 
+            'as a military police officer, and served a tour as a provost marshal investigator. Vietnam was his second service ' + 
+            '(he had previously worked as military police in South Korea) and he was assigned to the Ninth Military ' +
+            'Police Company of the Ninth Infantry division.', 
             location: {
                 center: [-87.59059, 41.78385],
                 zoom: 17.40,
-                pitch: 60,
-                bearing: 0
+                pitch: 54,
+                bearing: 1.60
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -41,23 +45,124 @@ var config = {
             ]
         },
         {
-            id: 'home-nyc',
+            id: 'chicago',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
-            image: './path/to/image/source.png',
-            description: 'My parent\'s home in NYC',
+            title: 'Chicago, 1970',
+            image: '',
+            description: 'In 1970, Burge joined the Chicago Police Department (CPD), where he rose in power ' + 
+            'to become Commander of Area 2. At least seventeen of his associates were also veterans of the U.S. ' +
+            'military, most from the Vietnam War.',
             location: {
                 center: [-73.95449, 40.82166],
                 zoom: 17.4,
-                pitch: 60,
-                bearing: -43.2
+                pitch: 54,
+                bearing: 1.60
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
+        {
+            id: 'area2-1982',
+            alignment: 'left',
+            hidden: false,
+            title: 'Area 2 Police Station, 1982',
+            image: '',
+            description: 'Andrew and Jackie Wilson, two of the earliest known survivors of torture at the ' + 
+            'hands of Chicago Police officer Jon Burge, shared a similar fate as the Rabbanis. They were arrested ' +
+            'in 1982 on a tip, coerced through torturing a suspect, that the Wilsons killed Chicago Police ' +
+            'Officers Fahey and O’Brien. They were tortured into confession; both brothers were connected to a ' +
+            'hand-cranked generator and given electric shocks. Their legal fights lasted decades: Andrew Wilson ' +
+            'ultimately died in prison in 2007. Jackie Wilson was released in 2018 and granted a certificate of ' +
+            'innocence in December 2020',
+            location: {
+                center: [-87.60398, 41.72948],
+                zoom: 17.4,
+                pitch: 54,
+                bearing: 1.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'area2-1983',
+            alignment: 'right',
+            hidden: false,
+            title: 'Area 2 Police Station, 1983',
+            image: '',
+            description: 'Darrell Cannon, tortured in 1983 and incarcerated for 24 years, testified that ' +
+            'the officers played a terrifying game of Russian Roulette on him, where they would seemingly ' +
+            'load a shotgun and stick it in his mouth, forcing him to pull the trigger. The officers then pulled ' +
+            'down his pants and repeatedly shocked him in the genitals with a cattle prod. “For me this was ' +
+            'the most sadistic act ever performed,” he told journalist Amanda Rivkin in 2016. “The feeling of ' +
+            'it is something that is indescribable. I still live with it today.” Sgt. John Byrne and Detectives ' +
+            'Peter Dignan, who worked closely with Burge and were both Marine Corps veterans, participated in ' +
+            'torturing and coercing his murder confession. Cannon was exonerated in 2004 and freed in 2007. His case ' +
+            'became a touchpoint for other Burge-related cases. Today, he organizes fellow survivors in the ' +
+            'reparations and torture justice movement.',
+            location: {
+                center: [-87.60413, 41.69223],
+                zoom: 17.4,
+                pitch: 54,
+                bearing: 1.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'area3',
+            alignment: 'left',
+            hidden: false,
+            title: 'Area 3 Police Station',
+            image: '',
+            description: 'Over the next two decades, Burge and his affiliates tortured more than 125 ' +
+            'Black people. Aided and abetted by prosecutors, city officials, and the Chicago Police ' +
+            'Department, these officers routinely framed innocent Black men for crimes they didn’t commit. ' +
+            'Often, these crimes were high profile, with an expectation and demand on police to catch the person responsible. ',
+            location: {
+                center: [-87.60413, 41.69223],
+                zoom: 17.4,
+                pitch: 60,
+                bearing: 42.40
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'area3-1988-1991',
+            alignment: 'right',
+            hidden: false,
+            title: 'Area 3 Police Station (1988-1991)',
+            image: '',
+            description: 'Burge moved to Area 3 in 1988. One of Burge’s contemporaries, Richard Zuley, ' +
+            'also joined the Chicago Police in 1970. Zuley,  who is most infamous for his direct link ' +
+            'to torture at Guantanamo, took a leave of absence from CPD from 1982-1987 to work in counterterrorism ' +
+            'for the U.S. Navy. It’s unclear what his counterterrorism work included, where he was ' +
+            'deployed, or how it may have influenced his behavior in Chicago.',
+            location: {
+                center: [-87.69508, 41.82261],
+                zoom: 17.4,
+                pitch: 54,
+                bearing: 1.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        
     ]
 };
